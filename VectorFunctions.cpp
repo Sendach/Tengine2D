045291eb1d57a6476810ef792803bc892062f690
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-13 13:50:29 +0200

    Put it in namespace t2d

--- VectorFunctions.cpp
+++ VectorFunctions.cpp
@@ -1,48 +1,51 @@
 #include "VectorFunctions.h"
 
-sf::Vector2f VectorFunctions::normalize(const sf::Vector2f& vector)
-{
-	float divisor = 1.0f / std::sqrtf(vector.x * vector.x + vector.y * vector.y);
-	return sf::Vector2f(vector.x * divisor, vector.y * divisor);
-}
-// Hiii
-sf::Vector3f VectorFunctions::normalize(const sf::Vector3f& vector)
-{
-	float divisor = 1.0f / std::sqrtf(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
-	return sf::Vector3f(vector.x * divisor, vector.y * divisor, vector.z * divisor);
-}
+namespace t2d {
 
-float VectorFunctions::dot(const sf::Vector2f& vector1, const sf::Vector2f& vector2)
-{
-	return vector1.x * vector2.x + vector1.y * vector2.y;
-}
+	sf::Vector2f VectorFunctions::normalize(const sf::Vector2f& vector)
+	{
+		float divisor = 1.0f / std::sqrtf(vector.x * vector.x + vector.y * vector.y);
+		return sf::Vector2f(vector.x * divisor, vector.y * divisor);
+	}
 
-sf::Vector3f VectorFunctions::cross(const sf::Vector2f& vector1, const sf::Vector2f& vector2)
-{
-	return sf::Vector3f(0.0f, 0.0f, vector1.x * vector2.y - vector1.y * vector2.x);
-}
+	sf::Vector3f VectorFunctions::normalize(const sf::Vector3f& vector)
+	{
+		float divisor = 1.0f / std::sqrtf(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+		return sf::Vector3f(vector.x * divisor, vector.y * divisor, vector.z * divisor);
+	}
 
-sf::Vector3f VectorFunctions::cross(const sf::Vector3f& vector1, const sf::Vector3f& vector2)
-{
-	return sf::Vector3f(vector1.y * vector2.z - vector1.z * vector2.y, vector1.z * vector2.x - vector1.x * vector2.z, vector1.x * vector2.y - vector1.y * vector2.x);
-}
+	float VectorFunctions::dot(const sf::Vector2f& vector1, const sf::Vector2f& vector2)
+	{
+		return vector1.x * vector2.x + vector1.y * vector2.y;
+	}
 
-sf::Vector2f VectorFunctions::v3tov2(const sf::Vector3f& vector)
-{
-	return sf::Vector2f(vector.x, vector.y);
-}
+	sf::Vector3f VectorFunctions::cross(const sf::Vector2f& vector1, const sf::Vector2f& vector2)
+	{
+		return sf::Vector3f(0.0f, 0.0f, vector1.x * vector2.y - vector1.y * vector2.x);
+	}
 
-sf::Vector3f VectorFunctions::v2tov3(const sf::Vector2f& vector)
-{
-	return sf::Vector3f(vector.x, vector.y, 0.0f);
-}
+	sf::Vector3f VectorFunctions::cross(const sf::Vector3f& vector1, const sf::Vector3f& vector2)
+	{
+		return sf::Vector3f(vector1.y * vector2.z - vector1.z * vector2.y, vector1.z * vector2.x - vector1.x * vector2.z, vector1.x * vector2.y - vector1.y * vector2.x);
+	}
 
-float VectorFunctions::length(const sf::Vector2f& vector)
-{
-	return std::sqrtf(vector.x * vector.x + vector.y * vector.y);
-}
+	sf::Vector2f VectorFunctions::v3tov2(const sf::Vector3f& vector)
+	{
+		return sf::Vector2f(vector.x, vector.y);
+	}
 
-float VectorFunctions::length2(const sf::Vector2f& vector)
-{
-	return vector.x * vector.x + vector.y * vector.y;
+	sf::Vector3f VectorFunctions::v2tov3(const sf::Vector2f& vector)
+	{
+		return sf::Vector3f(vector.x, vector.y, 0.0f);
+	}
+
+	float VectorFunctions::length(const sf::Vector2f& vector)
+	{
+		return std::sqrtf(vector.x * vector.x + vector.y * vector.y);
+	}
+
+	float VectorFunctions::length2(const sf::Vector2f& vector)
+	{
+		return vector.x * vector.x + vector.y * vector.y;
+	}
 }
--- VectorFunctions.h
+++ VectorFunctions.h
@@ -4,27 +4,28 @@
 #include <SFML\graphics.hpp>
 #include <cmath>
 
-class VectorFunctions 
-{
-public: 
-	static sf::Vector2f normalize(const sf::Vector2f& vector);
+namespace t2d {
 
-	static sf::Vector3f normalize(const sf::Vector3f& vector);
+	class VectorFunctions
+	{
+	public:
+		static sf::Vector2f normalize(const sf::Vector2f& vector);
 
-	static float dot(const sf::Vector2f& vector1, const sf::Vector2f& vector2);
+		static sf::Vector3f normalize(const sf::Vector3f& vector);
 
-	static sf::Vector3f cross(const sf::Vector2f& vector1, const sf::Vector2f& vector2);
+		static float dot(const sf::Vector2f& vector1, const sf::Vector2f& vector2);
 
-	static sf::Vector3f cross(const sf::Vector3f& vector1, const sf::Vector3f& vector2);
+		static sf::Vector3f cross(const sf::Vector2f& vector1, const sf::Vector2f& vector2);
 
-	static sf::Vector2f v3tov2(const sf::Vector3f& vector);
+		static sf::Vector3f cross(const sf::Vector3f& vector1, const sf::Vector3f& vector2);
 
-	static sf::Vector3f v2tov3(const sf::Vector2f& vector);
+		static sf::Vector2f v3tov2(const sf::Vector3f& vector);
 
-	static float length(const sf::Vector2f& vector);
+		static sf::Vector3f v2tov3(const sf::Vector2f& vector);
 
-	static float length2(const sf::Vector2f& vector);
-};
+		static float length(const sf::Vector2f& vector);
 
-
+		static float length2(const sf::Vector2f& vector);
+	};
+}
 #endif
