38901c3ec723283edac3b32cf9f0b98b00f1cb18
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-13 14:25:56 +0200

    Added 3 Functions in ResourceManager
    Added CreateSprite, CreateSpriteCI, CreateText.

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -2,6 +2,22 @@
 
 namespace t2d {
 
+	//
+	static void createSprite(const std::string textureName, const std::string spritePath)
+	{
+		SpriteCache::setupSprite(textureName, spritePath);
+	}
+	//
+	static void createSpriteCI(const std::string textureName, std::string spritePath, sf::IntRect cropped)
+	{
+		SpriteCache::setupSpriteCI(textureName, spritePath, cropped);
+	}
+	//
+	static void createText(const std::string textName, const std::string fontPath)
+	{
+		TextCache::setupText(textName, fontPath);
+	}
+
 	// Returns the sprite bound to the user-given name.
 	sf::Sprite& ResourceManager::getSprite(std::string spriteName)
 	{
--- ResourceManager.h
+++ ResourceManager.h
@@ -19,6 +19,12 @@ namespace t2d {
 		static sf::Sprite& getSprite(std::string spriteName);
 		// Returns the text bound to the user-given name.
 		static sf::Text& getText(std::string textName);
+		// Creates a sprite using TextureCache.h & CreateSprite.h
+		static void createSprite(const std::string textureName, const std::string spritePath);
+		// Creates a (cropped)sprite using SpriteCache.h & CreateSprite.h
+		static void createSpriteCI(const std::string textureName, std::string spritePath, sf::IntRect cropped);
+		// Creates a text using TextCache.h & CreateText.h
+		static void createText(const std::string textName, const std::string fontPath);
 
 	private:
 
