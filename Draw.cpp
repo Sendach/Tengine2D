4319c4698e258d554eebc637e71fa1eebca09e61
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-14 20:30:27 +0200

    Updated class to draw Engine's Sprite & Text Class types.

--- Draw.cpp
+++ Draw.cpp
@@ -5,3 +5,15 @@ void Draw::draw(sf::RenderWindow& window, const sf::Drawable& drawable)
 {
 	window.draw(drawable);
 }
+
+// Draws user-given sprite. Using the Engine defined sprite.
+void Draw::draw(sf::RenderWindow& window, const t2d::Sprite& sprite)
+{
+	window.draw(sprite._sprite);
+}
+
+// Draws user-given text. Using the engine defined Text.
+void Draw::draw(sf::RenderWindow& window, const t2d::Text& text)
+{
+	window.draw(text._text);
+}
--- Draw.h
+++ Draw.h
@@ -5,13 +5,20 @@
 #include <SFML\Graphics\RenderWindow.hpp>
 #include <SFML\Graphics\Drawable.hpp>
 
+// Classes
+#include "Sprite.h"
+#include "Text.h"
 // A static class that Window.h uses to draw.
 class Draw
 {
 public:
 
-	// Draws everything on the screen.
+	// Draws A SFML sprite/text on the screen.
 	static void draw(sf::RenderWindow& window, const sf::Drawable& drawable);
+	// Draws user-given sprite. Using the Engine defined sprite.
+	static void draw(sf::RenderWindow& window, const t2d::Sprite& sprite);
+	// Draws user-given text. Using the engine defined Text.
+	static void draw(sf::RenderWindow& window, const t2d::Text& text);
 
 private:
 
