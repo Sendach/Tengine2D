b78b399447ea93613feb3fdc8591839ab2d77a1c
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-14 12:22:21 +0200

    Added Set & Get functions.
    Set & Get functions were missing for Most of the variables.

--- Window.cpp
+++ Window.cpp
@@ -6,42 +6,111 @@ namespace t2d {
 sf::RenderWindow Window::_window;
 int Window::_screenWidth;
 int Window::_screenHeight;
+std::string Window::_windowName;
+Window::Style Window::_style;
 
 	// Creates a window.
 	void Window::create(int screenWidth, int screenHeight, std::string windowName)
 	{
 		_window.create(sf::VideoMode(screenWidth, screenHeight), windowName);
+
+		_screenWidth = screenWidth;
+		_screenHeight = screenHeight;
+		_windowName = windowName;
 	}
 
 	// Creates a window, with an added style, ex. Fullscreen.
-	//void Window::create(int screenWidth, int screenHeight, std::string windowName, int Style)
-	//{
-		//_window.create(sf::VideoMode(screenWidth, screenHeight), windowName, sf::Style::Fullscreen);
-	//}
+	void Window::create(int screenWidth, int screenHeight, std::string windowName, Window::Style style)
+	{
+		_window.create(sf::VideoMode(screenWidth, screenHeight), windowName, style);
+	}
 
 	// Draw to the screen.
-	void Window::draw(sf::Sprite& sprite)
+	void Window::draw(const sf::Drawable& drawable)
 	{
-		Draw::draw(_window, sprite);
+		Draw::draw(_window, drawable);
 	}
+	
 	// Draws.
 	void Window::clear()
 	{
 		_window.clear();
 	}
+	
 	/// Displays everything on the screen.
 	void Window::display()
 	{
 		_window.display();
 	}
+	
 	// Closes The Screen
 	void Window::close()
 	{
 		_window.close();
 	}
+	
 	// Pops the event on top of the event queue, if any, and returns it.
-	bool Window::pollEvent(sf::Event& _event)
+	bool Window::pollEvent()
 	{
-		return _window.pollEvent(_event);
+		return _window.pollEvent(t2d::Event::getEvent());
+	}
+
+	// Set screen width.
+	void Window::setScreenWidth(int screenWidth)
+	{
+		_window.setSize(sf::Vector2u(screenWidth, _screenHeight));
+		_screenWidth = screenWidth;
+	}
+
+	// Get screen width.
+	int Window::getScreenWidth()
+	{
+		return _screenWidth;
+	}
+
+	// Set screen height.
+	void Window::setScreenHeight(int screenHeight)
+	{
+		_window.setSize(sf::Vector2u(_screenWidth, screenHeight));
+		_screenHeight = screenHeight;
+	}
+
+	// Get screen Height.
+	int Window::getScreenHeight()
+	{
+		return _screenHeight;
+	}
+
+	// Set name of window.
+	void Window::setWindowName(std::string windowName)
+	{
+		_window.setTitle(windowName);
+		_windowName = windowName;
+	}
+
+	// Get name of window.
+	std::string Window::getWindowName()
+	{
+		return _windowName;
+	}
+	
+	// Set window style.
+	void Window::setWindowStyle(Style style)
+	{
+		/* Currently No official way to change the style */
+		_style = style;
+	}
+
+	// Get window style.
+	Window::Style Window::getWindowStyle()
+	{
+		return _style;
+	}
+
+	// Set window Size
+	void Window::setSize(int screenWidth, int screenHeight)
+	{
+		_window.setSize(sf::Vector2u(screenWidth, screenHeight));
+
 	}
 }
--- Window.h
+++ Window.h
@@ -9,6 +9,7 @@
 
 // Classes.
 #include "Draw.h"
+#include "Event.h"
 
 namespace t2d {
 
@@ -17,12 +18,21 @@ namespace t2d {
 	{
 	public:
 
+		// Different styles to create the window with.
+		enum  Style	{
+			None = 0,
+			Titlebar = 1 << 0,
+			Resize = 1 << 1,
+			Close = 1 << 2,
+			Fullscreen = 1 << 3
+		};
+
 		// Creates a window.
 		static void create(int screenWidth, int screenHeight, std::string windowName);
 		// Creates a window, with an added style, ex. Fullscreen.
-		static void create(int screenWidth, int screenHeight, std::string windowName,  int Style);
+		static void create(int screenWidth, int screenHeight, std::string windowName, Window::Style style);
 		// Draw to the screen.
-		static void draw(sf::Sprite& sprite);
+		static void draw(const sf::Drawable& drawable);
 		// Clears the screens.
 		static void clear();
 		// Displays everything on the screen.
@@ -30,7 +40,7 @@ namespace t2d {
 		// Closes The Screen.
 		static void close();
 		// Pops the event on top of the event queue, if any, and returns it.
-		static bool pollEvent(sf::Event& _event);
+		static bool pollEvent();
 
 		// Settings & Getters.
 		
@@ -44,6 +54,21 @@ namespace t2d {
 		// Get screen Height.
 		static int getScreenHeight();
 
+		// Set name of window.
+		static void setWindowName(std::string windowName);
+		// Get name of window.
+		static std::string getWindowName();
+
+		// Set window style.
+		static void setWindowStyle(Style style);
+		// Get window style.
+		static Style getWindowStyle();
+
+		// Set window Size
+		static void setSize(int screenWidth, int screenHeight);
+		// Note: getSize - Returns a tuple?
+			
+		
 		
 	private:
 		// Unused Default Constructor.
@@ -52,6 +77,10 @@ namespace t2d {
 		static int _screenWidth;
 		// Height of the screen.
 		static int _screenHeight;
+		// Name of the window.
+		static std::string _windowName;
+		// window Style.
+		static Style _style;
 		// The Window.
 		static sf::RenderWindow _window;
 
