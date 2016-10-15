ec654bb0fefa05169bc839d2b87d8ecbba715e80
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-15 15:02:34 +0200

    Removed namespace t2d

--- Draw.cpp
+++ Draw.cpp
@@ -1,22 +1,15 @@
 #include "Draw.hpp"
 
-namespace t2d {
 
-	// Draws everything on the screen.
-	void Draw::draw(sf::RenderWindow& window, const sf::Drawable& drawable)
-	{
-		window.draw(drawable);
-	}
 
-	// Draws user-given sprite. Using the Engine defined sprite.
-	void Draw::draw(sf::RenderWindow& window, const t2d::Sprite& sprite)
-	{
-		window.draw(sprite._sprite);
-	}
+// Draws user-given sprite. Using the Engine defined sprite.
+void Draw::draw(sf::RenderWindow& window, const Sprite& sprite)
+{
+	window.draw(sprite._sprite);
+}
 
-	// Draws user-given text. Using the engine defined Text.
-	void Draw::draw(sf::RenderWindow& window, const t2d::Text& text)
-	{
-		window.draw(text._text);
-	}
+// Draws user-given text. Using the engine defined Text.
+void Draw::draw(sf::RenderWindow& window, const Text& text)
+{
+	window.draw(text._text);
 }
