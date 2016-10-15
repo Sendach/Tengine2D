6b5ded5ae714ee3d621730ba34b2c3aea0e2e99e
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-16 01:56:26 +0200

    Now returns local Sprite

--- CreateSprite.cpp
+++ CreateSprite.cpp
@@ -2,14 +2,15 @@
 // C++ Standard Library
 #include <iostream>
 
-
-// Initialize Static variables
-std::map<std::string, Texture> CreateSprite::_texture;
-Sprite CreateSprite::_sprite;
+// A map which is used to bind and saves a user-given name to a new texture.
+static std::map< std::string, Texture> _texture;
 
 // Creates a sprite and loads in the entire image.
-Sprite& CreateSprite::createSprite(const std::string textureName, const std::string texturePath)
+Sprite CreateSprite::createSprite(const std::string textureName, const std::string texturePath)
 {
+	// Create sprite
+	Sprite _sprite;
+
 	// Uses our std::map to bind a user-given name to a new texture.
 	_texture[textureName] = Texture();
 	// Loads in the image/sprite from the user-given path.
@@ -32,8 +33,11 @@ Sprite& CreateSprite::createSprite(const std::string textureName, const std::str
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 // Creates a sprite and loads in a smaller part of the image, a cropped version. CI = Cropped Image.
-Sprite& CreateSprite::createSpriteCI(const std::string textureName, std::string texturePath, sf::IntRect cropped)
+Sprite CreateSprite::createSpriteCI(const std::string textureName, std::string texturePath, sf::IntRect cropped)
 {
+	// Create sprite
+	Sprite _sprite;
+
 	// Uses our std::map to bind a user-given name to a new texture.
 	_texture[textureName] = Texture();
 	// Loads in the image/sprite with a from the user-given path. Cropping the image using sf::IntRect 
--- CreateSprite.hpp
+++ CreateSprite.hpp
@@ -19,19 +19,17 @@ class CreateSprite
 public:
 
 	// Creates a sprite and loads in the entire image.
-	static Sprite& createSprite(const std::string textureName, const std::string path);
+	static Sprite createSprite(const std::string textureName, const std::string path);
 
 	// Creates a sprite and loads in a smaller part of the image, a cropped version. CI = Cropped Image.
-	static Sprite& createSpriteCI(const std::string textureName, const std::string path, sf::IntRect cropped);
+	static Sprite createSpriteCI(const std::string textureName, const std::string path, sf::IntRect cropped);
 
 private:
 
 	// Unused Default constructor
 	CreateSprite() {}
-	// A map which is used to bind and saves a user-given name to a new texture.
-	static std::map< std::string, Texture> _texture;
-	// When the sprite is created it is bound to this variable, which is then set to put in the SpriteCache.
-	static Sprite _sprite;
+	
+	
 
 };
 
