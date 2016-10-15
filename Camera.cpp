cc0b61050b5cfce331f0e52cc9416541200f6bf2
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-15 14:57:54 +0200

    Created Camera class.
    Haven't done too much with it yet, still trying to figure things out.

--- Camera.cpp
+++ Camera.cpp
@@ -1,11 +1,32 @@
 #include "Camera.hpp"
 
+namespace t2d {
 
-Camera::Camera()
-{
+	void Camera::create(sf::FloatRect rect)
+	{
+		_camera.reset(rect);
+	}
+
+	//
+	void Camera::zoom(float factor)
+	{
+		_camera.zoom(factor);
+	}
+
+	// 
+	void Camera::setSize(float width, float height)
+	{
+		_camera.setSize(width, height);
+		_width = width;
+		_height = height;
+	}
+
+	// 
+	const t2d::Vector2f& Camera::getSize()
+	{
+		return t2d::Vector2f(_camera.getSize().x, _camera.getSize().y);
+	}
+
+
 }
 
-
-Camera::~Camera()
-{
-}
--- Camera.hpp
+++ Camera.hpp
@@ -2,10 +2,54 @@
 #ifndef CAMERA_HPP
 #define CAMERA_HPP
 
-class Camera
-{
-public:
-	Camera();
-	~Camera();
-};
+// SFML.
+#include <SFML\Graphics\View.hpp>
+#include <SFML\Graphics\Rect.hpp>
+
+// Engine Classes
+#include "Vector2.hpp"
+#include "Window.hpp"
+
+namespace t2d {
+
+	class Camera
+	{
+	public:
+		Camera() { }
+		//
+		Camera(sf::FloatRect rect)
+		{
+			_camera.reset(rect);
+		}
+
+		/*static t2d::Camera getCamera()
+		{
+			return 
+		}*/
+
+		// Creates a Camera.
+		void create(sf::FloatRect rec);
+
+		// Zooms in if factor is a value above 1.0f, zooms out if vice versa.
+		void zoom(float factor);
+
+		// Setters & Getters
+
+		// Sets The size of the camera view.
+		void setSize(float width, float height);
+		//  Returns size of the camera view.
+		 const t2d::Vector2f& getSize();
+		
+
+	private:
+
+		// The actual camera
+		sf::View _camera;
+		// Width of camera view
+		float _width;
+		// Height of camera view
+		float _height;
+
+	};
+}
 #endif
