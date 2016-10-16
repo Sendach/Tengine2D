35aa430791b0653d694c8ea9fb2f3192f942d8d4
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-17 00:43:30 +0200

    Added comment

--- GameState.hpp
+++ GameState.hpp
@@ -2,6 +2,10 @@
 #ifndef GAMESTATE_HPP
 #define GAMESTATE_HPP
 
+
+/////////////////////////////////////////////////////////////////////
+// Change this class to a gameState class - http://gamedev.stackexchange.com/questions/13244/game-state-management-techniques //
+////////////////////////////////////////////////////////////////////
 namespace t2d {
 
 	// An enum class that holds the game states.
