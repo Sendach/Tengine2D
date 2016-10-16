097776228cf658241b1e2ac87cfb94385d377fd9
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-17 00:53:43 +0200

    Added BoxCollider class

---
+++ BoxCollider.cpp
@@ -0,0 +1,6 @@
+#include "BoxCollider.hpp"
+
+namespace t2d {
+
+
+}
---
+++ BoxCollider.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef BOXCOLLIDER_HPP
+#define BOXCOLLIDER_HPP
+
+
+namespace t2d {
+
+	// A Class that creates a tight rectangle box around a sprite, 
+	class BoxCollider
+	{
+	public:
+		BoxCollider() { }
+
+	};
+}
+#endif
