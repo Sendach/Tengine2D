9e5517bf6ec7095b345f56382c95546ca610df29
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-18 20:19:31 +0200

    Fixed update function

--- PlayerInputComponent.cpp
+++ PlayerInputComponent.cpp
@@ -17,19 +17,19 @@ void PlayerInputComponent::update(const GameObject& player, float delta)
 {
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::D))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(velocity * delta, 0.0f);
+		t2d::ResourceManager::getSprite(player.getName()).translate(velocity * delta, 0.0f);
 	}
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::A))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(-velocity * delta, 0.0f);
+		t2d::ResourceManager::getSprite(player.getName()).translate(-velocity * delta, 0.0f);
 	}
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::S))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(0.0f, velocity * delta);
+		t2d::ResourceManager::getSprite(player.getName()).translate(0.0f, velocity * delta);
 	}
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::W))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(0.0f, -velocity * delta);
+		t2d::ResourceManager::getSprite(player.getName()).translate(0.0f, -velocity * delta);
 	}
 }
 
