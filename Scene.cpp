f17d5c370c53ea7de8cdac3d2d1c4366b092b46f
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-18 17:01:54 +0200

    Scene Function render now renders all of the GameObjects

--- Scene.cpp
+++ Scene.cpp
@@ -4,9 +4,9 @@
 #include "GameObject.hpp"
 
 // Creates a game object. Returns it.
-std::shared_ptr<GameObject> Scene::createGameObject(std::string name, std::string tag, std::string layer)
+std::shared_ptr<GameObject> Scene::createGameObject(std::string path, std::string name, std::string tag, std::string layer)
 {
-	std::shared_ptr<GameObject> m_GameObject = std::make_shared<GameObject>(name, tag, layer);
+	std::shared_ptr<GameObject> m_GameObject = std::make_shared<GameObject>(path, name, tag, layer);
 
 	m_GameObjects.push_back(m_GameObject);
 
@@ -25,18 +25,22 @@ void Scene::loadScene()
 }
 
 // Update logic for this scene.
-void Scene::update()
+void Scene::update(float delta)
 {
-	for (auto& m_GameObjects : m_GameObjects)
+	for (auto& i : m_GameObjects)
 	{
-		m_GameObjects->update();
+		i->update(delta);
 	}
 }
 
 // Draw the entire scene on the screen.
-void Scene::render()
+// 
+void Scene::render(t2d::Window& window)
 {
-
+	for (auto& i : m_GameObjects)
+	{
+		i->render(window);
+	}
 }
 
 // Finds a GameOject by name and returns it. Only returns active GameObjects.
@@ -76,4 +80,15 @@ std::vector<std::shared_ptr<GameObject>>& Scene::findGameObjectsWithTag(std::str
 	}
 
 	return tags;
+}
+
+Sprite& Scene::createSprite(const const std::string textureName, const std::string spritePath)
+{
+	return t2d::ResourceManager::createSprite(textureName, spritePath);
+}
+
+// Returns the sprite bound to the user-given name
+Sprite& Scene::getSprite(std::string spriteName)
+{
+	return t2d::ResourceManager::getSprite(spriteName);
 }
--- Scene.hpp
+++ Scene.hpp
@@ -8,6 +8,8 @@
 #include <string>
 
 // Engine Classes
+#include "ResourceManager.hpp"
+#include "Window.hpp"
 // Forward Declaration
 extern class GameObject;
 
@@ -19,17 +21,24 @@ class Scene
 public:
 	Scene() { }
 	
+	///////////// Functions ///////////////
+
+	// Create a function that creates the window. 
+	// The user can obviously change its functionality.
+
 	// Load all of the data and graphics that this scene needs to function.
 	void loadScene();
 
 	// Update logic for this scene.
-	void update();
+	void update(float delta);
 
+	// Create a Graphic Class that Draws everything in the scene
+	// Graphics m_Graphics; m_Graphics->update(*this); 
 	// renders the entire scene on the screen.
-	void render();
+	void render(t2d::Window& window);
 
 	// Creates a game object
-	std::shared_ptr<GameObject> createGameObject(std::string name = "", std::string tag = "Untagged", std::string layer = "Default");
+	std::shared_ptr<GameObject> createGameObject(std::string path = "", std::string name = "", std::string tag = "Untagged", std::string layer = "Default");
 
 	// Finds a GameOject by name and returns it. Only returns active GameObjects.
 	std::shared_ptr<GameObject>& find(std::string name);
@@ -40,9 +49,24 @@ public:
 	// Returns a vector of active GameObjects with user-given tag. 
 	std::vector<std::shared_ptr<GameObject>>& findGameObjectsWithTag(std::string tag);
 
-	// Vector that holds all of the objects in the game
+
+
+
+
+	///////////// Variables ///////////////
+
+	// Vector that holds all of the objects in the Scene
 	std::vector<std::shared_ptr<GameObject>> m_GameObjects;
-	
+
+	// Vector that holds all the sprites in the scene
+	Sprite& createSprite(const const std::string textureName, const std::string spritePath);
+	// Returns the sprite bound to the user-given name
+	Sprite& getSprite(std::string spriteName);
+
+	// Perhaps move all of this to some kind of asset manager class?
+	// Our Asset Caches
+	SpriteCache m_Spritecache;
+	TextCache m_Textcache;
 	
 };
 #endif
