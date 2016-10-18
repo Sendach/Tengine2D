c016f438941dfd90560bad1800a1976566aa6281
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-18 20:19:18 +0200

    Added draw function. This class now draws the sprite.

--- SpriteRendererComponent.cpp
+++ SpriteRendererComponent.cpp
@@ -5,9 +5,31 @@
 
 SpriteRendererComponent::SpriteRendererComponent(std::string path, std::string name)
 {
+	// Initialize Variables
 	m_Path = path;
 	m_SpriteName = name;
 	m_ComponentName = "Sprite Renderer";
+
+	// Load sprite with user-given name and path
 	t2d::ResourceManager::createSprite(name, path);
 }
 
+void SpriteRendererComponent::update(const GameObject& gameObject, float delta)
+{
+	
+}
+
+const std::shared_ptr<Component>& SpriteRendererComponent::getComponent() const
+{
+	return std::make_shared<SpriteRendererComponent>();
+}
+
+const std::string SpriteRendererComponent::getName() const
+{
+	return m_ComponentName;
+}
+
+void SpriteRendererComponent::draw(t2d::Window& window)
+{
+	window.draw(t2d::ResourceManager::getSprite(this->m_SpriteName));
+}
--- SpriteRendererComponent.hpp
+++ SpriteRendererComponent.hpp
@@ -4,36 +4,32 @@
 
 // Engine Classes
 #include "Sprite.hpp"
-#include "Component.hpp"
+#include "GraphicsComponent.hpp"
+#include "Window.hpp"
 
 // C++ Standard Library
 #include <string>
 
-class SpriteRendererComponent : public Component
+class SpriteRendererComponent : public GraphicsComponent
 {
 public:
 	SpriteRendererComponent(std::string path = "", std::string name = "");
 	
-	virtual void update(const GameObject& gameObject, float delta)
-	{
-
-	}
-
-	virtual const std::shared_ptr<Component>& getComponent() const
-	{
-		return std::make_shared<SpriteRendererComponent>();
-	}
-
-	virtual const std::string getName() const
-	{
-		return m_ComponentName;
-	}
-
+	virtual void update(const GameObject& gameObject, float delta);
+	
+	virtual const std::shared_ptr<Component>& getComponent() const;
+	
+	virtual const std::string getName() const;
+	
+	void draw(t2d::Window& window);
 	// The sprite the component renders
 	Sprite m_Sprite;
 	
+	// Path to load sprite
 	std::string m_Path;
+	// Name of the sprite
 	std::string m_SpriteName;
+	// Name of this component
 	std::string m_ComponentName;
 };
 
