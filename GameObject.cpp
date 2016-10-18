792067f6cfdebc2c086d5e1dfc4c856155ce01f6
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-18 17:02:52 +0200

    Added variables for theGameObjects, as well as the SpriteRendererComponent

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,48 +1,109 @@
 #include "GameObject.hpp"
 
+// Engine Classes
+#include "Scene.hpp"
+
 // C++ Standard Library
 #include <algorithm>
 #include <iostream>
 
-#include "Scene.hpp"
 
+
+// Called when Object is created using createGameObject() in the Scene class.
 GameObject::GameObject()
 {
+	// Initialize variables
 	m_Name = " ";
 	m_Tag = "Untagged";
 	m_Layer = "Default";
 	m_Scene = nullptr;
+	m_Active = true;
+	m_EnableRender = true;
+
+	// Initializing Default Components
+	m_SpriteRenderer = std::make_shared<SpriteRendererComponent>();
+	m_Components.push_back(m_SpriteRenderer);
+
+	// Initialize the rest of the components, which the player can add.
+	initializeAllComponents();
 }
 
 // Called when Object is created using createGameObject() in the Scene class.
-GameObject::GameObject(std::string name, std::string tag, std::string layer)
+GameObject::GameObject(std::string path, std::string name, std::string tag, std::string layer)
 {
+	// Initialize variables
 	m_Name = name;
 	m_Tag = tag;
+	m_Layer = layer;
 	m_Scene = nullptr;
+	m_Active = true;
+	m_EnableRender = true;
+
+	// Initializing Default Components
+	m_SpriteRenderer = std::make_shared<SpriteRendererComponent>(path, name);
+	m_Components.push_back(m_SpriteRenderer);
+
+	// Initialize the rest of the components, which the player can add.
+	initializeAllComponents();
 }
 
 // Updates Everything. All components. A gameobjects input, rendering, physics etc.
-void GameObject::update()
+void GameObject::update(float delta)
 {
-	for (auto& m_Components : m_Components)
+	for (auto& i : m_Components)
 	{
-		m_Components->update(*this);
+		i->update(*this, delta);
 	}
 }
 
-// Allows User to add components to their GameObject.
-void GameObject::addComponent(std::shared_ptr<Component> component)
+// Draws the game object
+void GameObject::render(t2d::Window& window)
 {
-	m_Components.push_back(component);
+	if (m_Active)
+	{
+		if (m_EnableRender)
+		{
+			window.draw(m_Scene->getSprite(m_SpriteRenderer->m_SpriteName));
+		}
+	}
+}
+
+// A function that Fills our vector that holds all components (all_components)
+// with the components that the engine has.
+void GameObject::initializeAllComponents()
+{
+	all_components.push_back(std::make_shared<PlayerInputComponent>());
+	all_components.push_back(m_SpriteRenderer);
+
+}
+
+// Allows User to add components to their GameObject.
+void GameObject::addComponent(std::string componentName)//std::shared_ptr<Component> component)
+{
+	for (auto& i : all_components)
+	{
+		if (i->getName() == componentName)
+		{
+			m_Components.push_back(i);
+		}
+	}
 }
 
 // Allows user to remove components of their GameObject 
-void GameObject::removeComponent(std::shared_ptr<Component> component)
+void GameObject::removeComponent(std::string componentName)
 {
-	// https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
-	m_Components.erase(std::remove(m_Components.begin(), m_Components.end(), component), m_Components.end());
+	for (auto& i : all_components)
+	{
+		if (i->getName() == componentName)
+		{
+			// https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
+			m_Components.erase(std::remove(m_Components.begin(), m_Components.end(), i), m_Components.end());
+		}
+	}
+	
+	
 }
+
 
 void GameObject::addedToScene(Scene* newScene)
 {
@@ -84,4 +145,28 @@ void GameObject::setLayer(std::string newLayer)
 const std::string GameObject::getLayer() const
 {
 	return m_Layer;
+}
+
+// Enables or disables rendering of GameObject
+void GameObject::setEnableRender(bool enable)
+{
+	m_EnableRender = enable;
+}
+
+// Returns the state of the rendering of the GameObject
+const bool GameObject::getEnableRender() const
+{
+	return m_EnableRender;
+}
+
+// Actives or disables the GameObject
+void GameObject::setActive(bool active)
+{
+	m_Active = active;
+}
+
+// Returns the state of the GameObject
+const bool GameObject::getActive() const
+{
+	return m_Active;
 }
--- GameObject.hpp
+++ GameObject.hpp
@@ -9,10 +9,10 @@
 
 // Engine Classes
 #include "Component.hpp"
-
 #include "PlayerInputComponent.hpp"
-
+#include "SpriteRendererComponent.hpp"
 #include "Scene.hpp"
+#include "Sprite.hpp"
 
 // Base Game Object class that we can use for every object int he game.
 // The class contains a vector of Components, which the user can use to add components such as InputComponent, or PhysicsComponent.
@@ -25,20 +25,27 @@ class GameObject
 public:
 	GameObject();
 	// Called when Object is created using createGameObject() in the Scene class.
-	GameObject(std::string name = "", std::string tag = "Untagged", std::string layer = "Default");
-
+	GameObject(std::string path = "" , std::string name = "", std::string tag = "Untagged", std::string layer = "Default");
+	
 	///////// Functions //////////
 
 	// Updates Everything. All components. A gameobjects input, rendering, physics etc.
-	void update();
+	void update(float delta);
+
+	// Draws the game object
+	void render(t2d::Window& window);
 
 	// Allows User to add components to their GameObject.
-	void addComponent(std::shared_ptr<Component> component);
+	void addComponent(std::string componentName);//std::shared_ptr<Component> component);
 	// Allows user to remove components of their GameObject
-	void removeComponent(std::shared_ptr<Component> component);
+	void removeComponent(std::string componentName);
 
+	// Updates our reference to scene
 	void addedToScene(Scene* newScene);
-
+	
+	// A function that Fills our vector that holds all components (all_components)
+	// with the components that the engine has.
+	void initializeAllComponents();
 
 	///////// Setters & Getters /////////
 
@@ -57,18 +64,37 @@ public:
 	// Gets the object layer
 	const std::string getLayer() const;
 
-	///////// Variables /////////
+	// Enables or disables rendering of GameObject
+	void setEnableRender(bool enable);
+	// Returns the state of the rendering of the GameObject
+	const bool getEnableRender() const;
 
-	
+	// Actives or disables the GameObject
+	void setActive(bool active);
+	// Returns the state of the GameObject
+	const bool getActive() const;
+
+	///////// Variables /////////
 
 	// We hold a reference to the scene so the game objects can query the scene information,
 	// such as determining if the player is visible to an enemy etc.
 	Scene* m_Scene;
-	
-
+	Sprite m_Sprite;
 private:
-	// Holds all of the components, e.g InputComponent, PhysicsComponent, RenderComponent.
+
+	// All objects have a sprite renderer by default. It renders the sprite.
+	std::shared_ptr<SpriteRendererComponent> m_SpriteRenderer;
+
+	// All objects have a transform by default. It holds the position, rotation and scale of a sprite.
+////	//std::shared_ptr<TransformComponent> m_Transform;
+
+	// Holds all ofthe components, e.g InputComponent, PhysicsComponent, RenderComponent.
 	std::vector<std::shared_ptr<Component>> m_Components;
+
+	// Holds all the components in the engine.
+	// When the player wants a component, he gives a name, and we can search the names of
+	// All the components in the engine, to find the one they want.
+	std::vector<std::shared_ptr<Component>> all_components;
 
 	// Name of the object.
 	std::string m_Name;
@@ -81,6 +107,15 @@ private:
 	// particular characteristics. Layers are primarily used to restrict operations such as collision
 	// so they are only applied to the groups of objects that are relevant.
 	std::string m_Layer;
+
+	// Whether or not the GameObject should be rendered.
+	bool m_EnableRender;
+
+	// Whether or not a GameObject is active (turned on or off)
+	bool m_Active;
+
+	
+	
 };
 
 #endif
