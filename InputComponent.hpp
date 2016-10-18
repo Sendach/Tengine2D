4b322678b43d2c6458c4979a3ccf01d576016cd4
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-18 17:01:32 +0200

    Added virtual functions

--- InputComponent.hpp
+++ InputComponent.hpp
@@ -5,11 +5,22 @@
 // Engine Classes
 #include "Component.hpp"
 
+// C++ Standard Libary
+#include <string>
+#include <memory>
+
 class InputComponent : public Component
 {
 public:
-	virtual void update(const GameObject& gameObject) = 0;
-	
+
+	// Updates Component
+	virtual void update(const GameObject& gameObject, float delta) = 0;
+
+	// Returns the component
+	virtual const std::shared_ptr<Component>& getComponent() const = 0;
+
+	virtual const std::string getName() const = 0;
+
 };
 
 #endif
