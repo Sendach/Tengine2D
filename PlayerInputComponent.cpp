841f7198d630a0956f533cf543ba4d7a5c251a57
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-18 17:01:25 +0200

    Added virtual functions and variables

--- PlayerInputComponent.cpp
+++ PlayerInputComponent.cpp
@@ -1,28 +1,48 @@
 #include "PlayerInputComponent.hpp"
+
+// Engine Classes
 #include "Keyboard.hpp"
 #include "ResourceManager.hpp"
 
+// Default constructor - Initializes info
 PlayerInputComponent::PlayerInputComponent()
 {
-
+	name = "PlayerInput";
+	amount_of_components++;
+	this->velocity = 400;
 }
 
-void PlayerInputComponent::update(const GameObject& player)
+// Updates Component
+void PlayerInputComponent::update(const GameObject& player, float delta)
 {
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::D))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(400 * 0.00023, 0.0f);
+		t2d::ResourceManager::getSprite("RedPlayer").translate(velocity * delta, 0.0f);
 	}
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::A))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(-400 * 0.00023, 0.0f);
+		t2d::ResourceManager::getSprite("RedPlayer").translate(-velocity * delta, 0.0f);
 	}
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::S))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(0.0f, 400 * 0.00023);
+		t2d::ResourceManager::getSprite("RedPlayer").translate(0.0f, velocity * delta);
 	}
 	if (t2d::Keyboard::isKeyPressed(t2d::Keyboard::W))
 	{
-		t2d::ResourceManager::getSprite("RedPlayer").translate(0.0f, -400 * 0.00023);
+		t2d::ResourceManager::getSprite("RedPlayer").translate(0.0f, -velocity * delta);
 	}
 }
+
+// Returns the component
+const std::shared_ptr<Component>& PlayerInputComponent::getComponent() const
+{
+	return std::make_shared<PlayerInputComponent>();
+	
+}
+
+// Returns the amount of components that we have
+const std::string PlayerInputComponent::getName() const
+{
+	return name;
+}
+
--- PlayerInputComponent.hpp
+++ PlayerInputComponent.hpp
@@ -2,20 +2,35 @@
 #ifndef PLAYERINPUTCOMPONENT_HPP
 #define PLAYERINPUTCOMPONENT_HPP
 
+// Engine Classes
 #include "InputComponent.hpp"
-
 #include "GameObject.hpp"
+
+// C++ Standard Library
+#include <string>
+#include <memory>
 
 class PlayerInputComponent : public InputComponent
 {
 public:
 
+	// Default constructor - Initializes info
 	PlayerInputComponent();
 
-	virtual void update(const GameObject& gameObject);
-	
+	// Updates Component
+	virtual void update(const GameObject& gameObject, float delta);
+
+	// Returns the component
+	virtual const std::shared_ptr<Component>& getComponent() const;
+
+	// Returns the amount of components that we have
+	virtual const std::string PlayerInputComponent::getName() const;
+
 private:
 
-
+	// Name of the component
+	std::string name;
+	// velocity of the Player
+	float velocity;
 };
 #endif
