752ca1305237dc8f2f256f8873cf8287eddd47ff
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-18 17:00:29 +0200

    Added common Component Functions

--- Component.cpp
+++ Component.cpp
@@ -1,2 +1,7 @@
 #include "Component.hpp"
 
+// Returns the amount of components that we have
+const int Component::getAmountOfComponent() const
+{
+	return amount_of_components;
+}
--- Component.hpp
+++ Component.hpp
@@ -5,11 +5,27 @@
 // Forward Declare the class, so it now it exists.
 class GameObject;
 
+// C++ Standard Library
+#include <memory>
+#include <string>
+
 class Component
 {
 public:
-	virtual	void update(const GameObject& gameObject) = 0;
+	// Updates component
+	virtual	void update(const GameObject& gameObject, float delta) = 0;
 
+	// Returns the component
+	virtual const std::shared_ptr<Component>& getComponent() const = 0;
+
+	// Returns the name of the component
+	virtual const std::string getName() const = 0;
+
+	// Returns the amount of components that we have
+	const int getAmountOfComponent() const;
+
+	// Holds the amount of components taht we have
+	int amount_of_components = 0;
 };
 
 #endif
