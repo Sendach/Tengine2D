1c29169fe8d943b049065699462b9f3d22c66cdb
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-20 01:43:08 +0200

    Added virtual functions

--- GraphicsComponent.hpp
+++ GraphicsComponent.hpp
@@ -2,13 +2,22 @@
 #ifndef GRAPHICSCOMPONENT_HPP
 #define GRAPHICSCOMPONENT_HPP
 
+// Engine Classes
 #include "Component.hpp"
+#include "Window.hpp"
 
 class GraphicsComponent : public Component
 {
 public:
 	GraphicsComponent();
 	
+	virtual void update(const GameObject& gameObject, float delta) = 0;
+
+	virtual const std::shared_ptr<Component>& getComponent() const = 0;
+
+	virtual const std::string getName() const = 0;
+
+	virtual void draw(t2d::Window& window) = 0;
 };
 
 #endif
