58e9b0814870c42ea26165ba38abc47f480e5c56
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-20 15:43:14 +0200

    Added functionality

--- InputManager.cpp
+++ InputManager.cpp
@@ -4,29 +4,81 @@
 InputManager::InputManager()
 {
 	m_Size = 15;
-	m_Axes = std::vector<Axes>(m_Size);
+	//m_Axis.reserve(m_Size);
 	initializeAxesNames();
 	
 	// Call the function in axes and set the appropriet name.
 	int counter = 0;
-	for (auto& i : m_Axes)
+	for (int i = 0; i < m_Size; i++)
 	{
-		i.setAxesName(m_AxesNames[counter]);
-		if (counter == 9) { counter = 0; }
+		m_Axis.push_back(Axes(m_AxisNames[counter]));
+		if (counter == 8) { counter = 0; }
 		counter++;
 	}
+	
 }
 
 // Initialize Access Names
 void InputManager::initializeAxesNames()
 {
-	m_AxesNames.push_back("Horizontal");
-	m_AxesNames.push_back("Vertical");
-	m_AxesNames.push_back("Fire1");
-	m_AxesNames.push_back("Fire2");
-	m_AxesNames.push_back("Fire3");
-	m_AxesNames.push_back("Jump");
-	m_AxesNames.push_back("MouseX");
-	m_AxesNames.push_back("MouseY");
-	m_AxesNames.push_back("Mouse ScrollWheel");
+	m_AxisNames.push_back("Horizontal");
+	m_AxisNames.push_back("Vertical");
+	m_AxisNames.push_back("Fire1");
+	m_AxisNames.push_back("Fire2");
+	m_AxisNames.push_back("Fire3");
+	m_AxisNames.push_back("Jump");
+	m_AxisNames.push_back("MouseX");
+	m_AxisNames.push_back("MouseY");
+	m_AxisNames.push_back("Mouse ScrollWheel");
+}
+
+// Returns true if axis exist and one of the buttons for that axis is being held down.
+bool InputManager::getButton(std::string axisName)
+{
+	for (auto& i : m_Axis)
+	{
+		if (axisName == i.getAxisName())
+		{
+			return i.getButton();
+		}
+	}
+	return false;
+	// Return error saying this Input button axisName is not setup.
+}
+
+// Returns true if axis exists and one of the buttons for that axis was pressed this frame.
+bool InputManager::getButtonDown(std::string axisName)
+{
+	for (auto& i : m_Axis)
+	{
+		if (axisName == i.getAxisName())
+		{
+			return i.getButtonDown();
+		}
+	}
+	return false;
+	// Return error saying this Input button axisName is not setup.
+}
+
+Axes& InputManager::getAxis(const std::string axisName)
+{
+	for (auto& i : m_Axis)
+	{
+		if (axisName == i.getAxisName())
+		{
+			return i;
+		}
+	}
+}
+
+const float InputManager::getAxisValue(std::string axisName)
+{
+	for (auto& i : m_Axis)
+	{
+		if (axisName == i.getAxisName())
+		{
+			return i.getAxisValue(axisName);
+		}
+	}
+	return 0;
 }
--- InputManager.hpp
+++ InputManager.hpp
@@ -5,6 +5,8 @@
 // Engine Classes
 #include "Axes.hpp"
 
+// C++ Standard Library
+#include <map>
 class InputManager
 {
 public:
@@ -12,10 +14,30 @@ public:
 
 	// Initialize Access Names
 	void initializeAxesNames();
+
+	// Returns true if axis exist and one of the buttons for that axis is being held down.
+	bool getButton(std::string axisName);
+
+	// Returns true if axis exists and one of the buttons for that axis was pressed this frame.
+	bool getButtonDown(std::string axisName);
+
+	// Returns instance of the virtual axis identified by axisName. So the user can change the buttons etc.
+	Axes& getAxis(const std::string axisName);
+
+	// Returns the value of the virtual axis identified by axisName
+	// In a keyboard & Joystick it just ranges between -1 and 1. 
+	// Keyboard: -1 being negative directio nand 1 being positive. 0 if the button is not pressed.
+	// Joystick: Ranges between -1 and 1 depending on how far the user pulls the stick. // Havent figured this one out yet
+	// Mouse: the mouse delta is multiplied by the axis sensitivity and the range is not -1...1.
+	const float getAxisValue(std::string axisName);
+
 private:
 	// The Amount of Axes/Buttons in the project. Default is 15.
 	int m_Size;
-	std::vector<Axes> m_Axes;
-	std::vector<std::string> m_AxesNames;
+	std::vector<Axes> m_Axis;
+	std::vector<std::string> m_AxisNames;
+
+	// So the InputManager can move the player accordingly.
+
 };
 #endif
