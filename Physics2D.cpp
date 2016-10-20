24503fe3828e3554b3c03cb879ada4a4735fce25
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-20 18:54:04 +0200

    Added class

---
+++ Physics2D.cpp
@@ -0,0 +1,8 @@
+#include "Physics2D.hpp"
+
+
+Physics2D::Physics2D()
+{
+}
+
+
---
+++ Physics2D.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef PHYSICS2D_HPP
+#define PHYSICS2D_HPP
+
+// Physics Engine 2D
+
+class Physics2D
+{
+public:
+	Physics2D();
+	
+};
+#endif
+
