a6ae566ce1605b67e34b676a4825c06b30292dc7
TarikNeaj <Tarik_lg3@hotmail.com>
2016-10-20 15:43:32 +0200

    Added functionality

--- Axes.cpp
+++ Axes.cpp
@@ -1,40 +1,104 @@
 #include "Axes.hpp"
 
 
-Axes::Axes()
+Axes::Axes(std::string axisName)
 {
-	
+	m_Name = axisName;
+
+	m_KeyboardValue = 0;
+	m_JoystickValue = 0;
+	m_MouseValue = 0;
+
+	setAxisButtons();
 }
 
 
-void Axes::setAxesName(std::string axesName)
+void Axes::setAxisName(std::string axesName)
 {
 	m_Name = axesName;
 }
 
-void Axes::setButtonNames()
+const std::string Axes::getAxisName() const
+{
+	return m_Name;
+}
+
+void Axes::setAxisButtons()
 {
 	if (m_Name == "Horizontal")
 	{
-		setNegativeButtonKey(m_Keyboard.Left);
-		setPositiveButtonKey(m_Keyboard.Right);
-		setAltNegativeButtonKey(m_Keyboard.A);
-		setAltPositiveButtonKey(m_Keyboard.D);
+		setNegativeButtonKey(t2d::Keyboard::Left);
+		setPositiveButtonKey(t2d::Keyboard::Right);
+		setAltNegativeButtonKey(t2d::Keyboard::A);
+		setAltPositiveButtonKey(t2d::Keyboard::D);
 	}
 	else if (m_Name == "Vertical")
 	{
-		setNegativeButtonKey(m_Keyboard.Up);
-		setPositiveButtonKey(m_Keyboard.Down);
-		setAltNegativeButtonKey(m_Keyboard.W);
-		setAltPositiveButtonKey(m_Keyboard.S);
+		setNegativeButtonKey(t2d::Keyboard::Up);
+		setPositiveButtonKey(t2d::Keyboard::Down);
+		setAltNegativeButtonKey(t2d::Keyboard::W);
+		setAltPositiveButtonKey(t2d::Keyboard::S);
 	}
 
+}
+
+bool Axes::getButton()
+{
+	if (t2d::Keyboard::isKeyPressed(getNegativeButtonKey()))
+	{
+		m_KeyboardValue = -1;
+		return true;
+	}
+	else if (t2d::Keyboard::isKeyPressed(getPositivebuttonKey()))
+	{
+		m_KeyboardValue = 1;
+		return true;
+	}
+	else if (t2d::Keyboard::isKeyPressed(getAltNegativeButtonKey()))
+	{
+		m_KeyboardValue = -1;
+		return true;
+	}
+	else if (t2d::Keyboard::isKeyPressed(getAltPositivebuttonKey()))
+	{
+		m_KeyboardValue = 1;
+		return true;
+	}
+
+	m_KeyboardValue = 0;
+	return false;
+}
+
+bool Axes::getButtonDown()
+{
+	if (t2d::Keyboard::keyPressed(getNegativeButtonKey()))
+	{
+		m_KeyboardValue = -1;
+		return true;
+	}
+	else if (t2d::Keyboard::keyPressed(getPositivebuttonKey()))
+	{
+		m_KeyboardValue = 1;
+		return true;
+	}
+	else if (t2d::Keyboard::keyPressed(getAltNegativeButtonKey()))
+	{
+		m_KeyboardValue = -1;
+		return true;
+	}
+	else if (t2d::Keyboard::keyPressed(getAltPositivebuttonKey()))
+	{
+		m_KeyboardValue = 1;
+		return true;
+	}
+
+	return false;
 }
 
 // Sets the key for the negative button key
 void Axes::setNegativeButtonKey(t2d::Keyboard::Key key)
 {
-	m_NegativeButton = key;
+	m_NegativeButtonKey = key;
 }
 
 // Returns the key for the negative button key
@@ -78,5 +142,13 @@ void Axes::setAltPositiveButtonKey(t2d::Keyboard::Key key)
 const t2d::Keyboard::Key Axes::getAltPositivebuttonKey() const
 {
 	return m_AltPositiveButtonKey;
+}
+
+const float Axes::getAxisValue(std::string axisName)
+{
+	
+	getButton();
+
+	return m_KeyboardValue;
 }
 
--- Axes.hpp
+++ Axes.hpp
@@ -14,25 +14,38 @@
 class Axes
 {
 public:
-	Axes();
+
+	// Creates an axis with a name
+	Axes(std::string axisName);
 
 	// When creating an instance of this class, this function will be called, which takes in
 	// The name of the axes, like "Horizontal" or "Vertical" or "Jump" etc. And depending
 	// On that name, All the buttons for each axes gets created with default values.
 	// Example - If an instance of this class is created using "Horizontal", then 
 	// NegativeButton will be the key LeftArrow, or A - PositiveButton will be RightArrow, or D
-	void setAxesName(std::string axesName);
-
 	// Depending on the name of the axis, the buttons will have different values.
-	void setButtonNames();
+	void setAxisButtons();
+
+	// When the user types Input::getAxis("Horizontal"), this function is called
+	// And it checks if the keys attached with the axis "Horizontal" are being held down.
+	bool getButton();
+
+	// When the user types Input::getAxis("Horizontal"), this function is called
+	// And it checks if the keys attached with the axis "Horizontal" were pressed this frame.
+	bool getButtonDown();
 
 	// Setters & Getters // 
+
+	// Sets name of Axis
+	void setAxisName(std::string axesName);
+	// Gets name of Axis
+	const std::string getAxisName() const;
 
 	// Sets the key for the negative button key
 	void setNegativeButtonKey(t2d::Keyboard::Key key);
 	// Returns the key for the negative button key
 	const t2d::Keyboard::Key getNegativeButtonKey() const;
-
+	
 	// Sets the key for the positive button key
 	void setPositiveButtonKey(t2d::Keyboard::Key key);
 	// Returns the key for the positive button key
@@ -48,35 +61,56 @@ public:
 	// Returns the key for the alternative positive button key
 	const t2d::Keyboard::Key getAltPositivebuttonKey() const;
 
+	// Returns the value of the virtual axis identified by axisName
+	// In a keyboard & Joystick it just ranges between -1 and 1. 
+	// Keyboard: -1 being negative directio nand 1 being positive. 0 if the button is not pressed.
+	// Joystick: Ranges between -1 and 1 depending on how far the user pulls the stick. // Havent figured this one out yet
+	// Mouse: the mouse delta is multiplied by the axis sensitivity and the range is not -1...1.
+	// This function also checks whether or not any button is pressed. So the user can just do something like
+	// float value = Input::getAxisValue("Horizontal") * speed(float) * delta;
+	// Since this function checks if a button is pressed. And if no button is pressed, getAxisValue returns 0.
+	// Meaning Input::getAxisValue("Horizontal") will be 0, and the entire calculation results in 0, because anything times 0 is 0.
+	// However, since it checks if a button is pressed, if it is, it returns 1. Meaning, the calculation is done.
+	// So if we would later do GameObject->trasnform->Translate(value, 0);
+	// It would move in the horizontal axis in the speed of speed * delta, if and only if one of the buttons
+	// Connected to the horizontal axis are being pressed.
+	const float getAxisValue(std::string axisName);
 
 private:
 	// Name of the Axes/Button
 	std::string m_Name;
+
 	// Axes/Button Going in the negative Axis
 	std::string m_NegativeButton;
 	t2d::Keyboard::Key m_NegativeButtonKey;
+
 	// Axes/Button Going in the positive Axis
 	std::string m_PositiveButton;
 	t2d::Keyboard::Key m_PositiveButtonKey;
+
 	// Alternative Axes/Button going in the negative axis
 	std::string m_AltNegativeButton;
 	t2d::Keyboard::Key m_AltNegativeButtonKey;
+
 	// Alternative Axes/Button going in the positive axis
 	std::string m_AltPositiveButton;
 	t2d::Keyboard::Key m_AltPositiveButtonKey;
+
 	// Whether or not gravity should affect the GameObject
 	std::string m_Gravity;
+
 	// Any positive or negative values that are less than this number will register as zero. Useful for joysticks.
 	std::string m_Dead;
+
 	// For keyboard input, a larger value will result in faster response time. A lower value will be more smooth. 
 	// For Mouse delta the value will scale the actual mouse delta.
 	std::string m_Sensitivity;
 
-	// Instance of Keyboard
-	t2d::Keyboard m_Keyboard;
-	
+	// Variables used by getAxisValue
 
+	int m_KeyboardValue;
+	float m_JoystickValue;
+	float m_MouseValue;
 
-	
 };
 #endif
